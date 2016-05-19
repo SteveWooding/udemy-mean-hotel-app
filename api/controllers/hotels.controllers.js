c322c326e0232714e5d253d459ce7e973279f8fc
Steven Wooding <steve@stevenwooding.com>
2016-05-19 17:38:10 +0100

    feat: Add pagination to /hotels using query strings

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -4,9 +4,29 @@ var hotelData = require('../data/hotel-data.json');
 // Controller to return all the hotel data.
 module.exports.hotelsGetAll = function(req, res) {
   console.log('GET the hotels');
+  console.log(req.query);
+
+  // Set defaults for paganation
+  var offset = 0;
+  var count = 5;
+
+  // Process any offset query string
+  if (req.query && req.query.offset) {
+    offset = parseInt(req.query.offset, 10);
+  }
+
+  // Process any count query string
+  if (req.query && req.query.count) {
+    count = parseInt(req.query.count, 10);
+  }
+
+  // Extract the requested data
+  var returnData = hotelData.slice(offset, offset + count);
+
+  // Define the response
   res
     .status(200)
-    .json(hotelData);
+    .json(returnData);
 };
 
 // Controller to return data on a singal hotel.
