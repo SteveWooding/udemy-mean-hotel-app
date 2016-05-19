ce1d621a648b2ee27062934883bfc187098a5336
Steven Wooding <steve@stevenwooding.com>
2016-05-19 15:29:20 +0100

    refactor: Put JSON route in its own file under routes directory

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+// Import routes directory
+var routes = require('./routes');
+
 // Define the port to run on
 app.set('port', 3000);
 
@@ -14,21 +17,8 @@ app.use(function(req, res, next) {
 // Define where the static files are located. An example of middleware.
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define a route for some JSON data
-app.get('/json', function(req, res) {
-  console.log('GET the json');
-  res
-    .status(200)
-    .json({ "jsonData": true });
-});
-
-// Define a route for sending a file
-app.get('/file', function(req, res) {
-  console.log('GET the file');
-  res
-    .status(200)
-    .sendFile(path.join(__dirname, 'app.js'));
-});
+// Use routes as defined in the routes sub-directory for /api urls.
+app.use('/api', routes);
 
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+router
+  .route('/json')
+  .get(function(req, res) {
+    console.log('GET the json');
+    res
+      .status(200)
+      .json({ "jsonData": true });
+  })
+  .post(function(req, res) {
+    console.log('POST the json route');
+    res
+      .status(200)
+      .json({ "jsonData": "POST received" });
+  });
+
+module.exports = router;
