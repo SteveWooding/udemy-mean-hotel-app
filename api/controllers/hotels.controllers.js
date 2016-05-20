ef6d6e5b169e6c7ae2fdab39da9cb67cdd49c803
Steven Wooding <steve@stevenwooding.com>
2016-05-20 16:15:24 +0100

    feat: Make the add hotel controller use the database

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -69,11 +69,28 @@ module.exports.hotelsAddOne = function(req, res) {
 
   // Get the database connection
   var db = dbconn.get();
-  console.log('db', db);
+  var collection = db.collection('hotels');
+  var newHotel;
 
   console.log('POST new hotel');
-  console.log(req.body);
-  res
-    .status(200)
-    .json(req.body);
+
+  if (req.body && req.body.name && req.body.stars) {
+    newHotel = req.body;
+    newHotel.stars = parseInt(req.body.stars, 10);
+
+    collection.insertOne(newHotel, function(err, response) {
+      console.log(response.ops);
+      res
+        .status(201)
+        .json(response.ops);
+    });
+
+  }
+  else
+  {
+    console.log('Data missing from body');
+    res
+      .status(400)
+      .json({ message: "Required data missing from body"});
+  }
 };
