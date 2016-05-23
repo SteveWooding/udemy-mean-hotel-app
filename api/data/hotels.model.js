d211b3d361b192aa31c38590b1b92fb85533d91c
Steven Wooding <steve@stevenwooding.com>
2016-05-23 10:44:36 +0100

    feat: Add review, room and location to the hotel schema

--- api/data/hotels.model.js
+++ api/data/hotels.model.js
@@ -1,5 +1,35 @@
 var mongoose = require('mongoose');
 
+// Define a schema for the reviews (will be a sub-document of hotel schema)
+var reviewSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+    required: true
+  },
+  review: {
+    type: String,
+    required: true
+  },
+  createOn: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+var roomSchema = new mongoose.Schema({
+  type: String,
+  number: Number,
+  description: String,
+  photos: [String],
+  price: Number
+});
+
 // Define a schmea for a hotel in the database
 var hotelSchema = new mongoose.Schema({
   name: {
@@ -15,7 +45,17 @@ var hotelSchema = new mongoose.Schema({
   description: String,
   photos: [String],
   currency: String,
-  services: [String]
+  services: [String],
+  reviews: [reviewSchema],
+  rooms: [roomSchema],
+  location: {
+    address: String,
+    coordinates: {
+      // Always store coordinates longitude (E/W), latitude (N/S) order
+      type: [Number],
+      index: '2dsphere'  // Helps Mongo search coordinates for nearby hotels
+    }
+  }
 });
 
 // Compile the model (called 'Hotel') from the above schema
