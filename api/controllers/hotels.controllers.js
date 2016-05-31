44f0180feed7b29c6484634bb9afa8b1d7624a41
Steven Wooding <steve@stevenwooding.com>
2016-05-31 11:10:58 +0100

    refactor: Change the add hotel controller to use Mongoose

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -162,33 +162,44 @@ module.exports.hotelsGetOne = function(req, res) {
 
 };
 
+// Helper function to covert ';' delimited string into an array, but return an
+// empty array if the string is empty.
+var _splitArray = function(input) {
+  var output;
+  if (input && input.length > 0) {
+    output = input.split(';');
+  } else {
+    output = [];
+  }
+  return output;
+};
+
 // Controller to add a hotel
 module.exports.hotelsAddOne = function(req, res) {
 
-  // Get the database connection
-  var db = dbconn.get();
-  var collection = db.collection('hotels');
-  var newHotel;
-
-  console.log('POST new hotel');
-
-  if (req.body && req.body.name && req.body.stars) {
-    newHotel = req.body;
-    newHotel.stars = parseInt(req.body.stars, 10);
-
-    collection.insertOne(newHotel, function(err, response) {
-      console.log(response.ops);
-      res
-        .status(201)
-        .json(response.ops);
+  Hotel
+    .create({
+      name: req.body.name,
+      description: req.body.description,
+      stars: parseInt(req.body.stars, 10),
+      services: _splitArray(req.body.services),
+      photos: _splitArray(req.body.photos),
+      currency: req.body.currency,
+      location: {
+        address: req.body.address,
+        coordinates: [parseFloat(req.body.lng), parseFloat(req.body.lat)]
+      }
+    }, function(err, hotel) {
+      if (err) {
+        console.log('Error creating hotel');
+        res
+          .status(400)
+          .json(err);
+      } else {
+        console.log('Hotel created', hotel);
+        res
+          .status(201)
+          .json(hotel);
+      }
     });
-
-  }
-  else
-  {
-    console.log('Data missing from body');
-    res
-      .status(400)
-      .json({ message: "Required data missing from body"});
-  }
 };
