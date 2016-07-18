4891571d225bdd7a21cfa90c23032d40fd259e48
Steven Wooding <steve@stevenwooding.com>
2016-07-18 16:33:14 +0100

    feat: Setup a basic Angular App
    Define a root route and the start of a controller.

---
+++ public/angular-app/app.js
@@ -0,0 +1,17 @@
+angular.module('meanhotel', ['ngRoute'])
+  .config(config)
+  .controller('HotelsController', HotelsController);
+
+function config($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'angular-app/hotels.html',
+      controller: HotelsController,
+      controllerAs: 'vm'
+    });
+}
+
+function HotelsController() {
+  var vm = this;
+  vm.title = 'MEAN Hotel App';
+}
