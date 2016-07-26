19bd76807c10d0217f0806fa9516e12b887b8248
Steven Wooding <steve@stevenwooding.com>
2016-07-26 16:34:21 +0100

    feat: Get data for 10 hotels from the API
    The names of the 10 hotels are displayed.

--- public/angular-app/hotel-list/hotel-list-controller.js
+++ public/angular-app/hotel-list/hotel-list-controller.js
@@ -1,6 +1,9 @@
 angular.module('meanhotel').controller('HotelsController', HotelsController);
 
-function HotelsController() {
+function HotelsController($http) {
   var vm = this;
   vm.title = 'MEAN Hotel App';
+  $http.get('/api/hotels?count=10').then(function(response) {
+    vm.hotels = response.data;
+  });
 }
